b41b2a28780f880b47a65dfd7139d9b43d76cf0b
Bruno Leo <BrunoLeo@users.noreply.github.com>
2022-12-13 23:52:10 -0300

    Aulas 13/12

---
+++ PHP/aula04/tipos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos PHP</title>
+    <link rel="stylesheet" href="_css/style.css">
+</head>
+<body>
+    <div>
+    <?php
+    $n=4;
+    $no="Leonardo";
+    echo $no;
+    ?>
+    </div>
+</body>
+</html>
